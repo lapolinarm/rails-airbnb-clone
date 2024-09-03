e8d7720296ae2304a3851370353ee760d6e4a3f3
Laura ramirez lrodar23m@icloud.com <lrodar23m@icloud.com>
2024-09-03 18:13:41 -0500

    se agrego los metodos del controller booking y las vitas edit index new show y el parcial form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,50 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
-
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
+    @room = Room.find(params[:id])
   end
 
   def create
-  end
+    @booking = current_user.bookings.build(booking_params)
+    @booking.room = Room.find(params[:room_id])
 
+    if @booking.save
+      redirect_to @booking, notice: 'Reserva creada exitosamente.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
   def edit
+  @booking = Booking.find(params[:id])
+  @room = @booking.room
   end
 
   def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Reserva actualizada exitosamente.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, notice: 'Reserva eliminada exitosamente.'
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
