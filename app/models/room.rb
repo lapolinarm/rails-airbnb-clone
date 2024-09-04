5c2ae3440de46d837e3019f46b86c181e18be212
Levi Apolinar <leviam51@gmail.com>
2024-09-03 23:21:16 -0500

    Added actions for rooms_controller, you can now see the results on the home page

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,13 @@ class Room < ApplicationRecord
   # Una habitación pertenece a un usuario y puede tener muchas reservas
   belongs_to :user
   has_many :bookings
+  has_many :reviews, through: :bookings
 
   # Validations
   validates :name, :description, :address, :capacity_max, :price, presence: true
+
+   # Instance Methods
+  def average_rating
+    reviews.average(:rating).to_f.round(2)
+  end
 end
